b12434e19e37c797735087f17a9963787bfbdff6
Your Name <you@example.com>
2025-04-07 15:02:29 -0500

    install script and zshrc original

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e  # Exit on errors
+
+# Detect brew
+if command -v brew &>/dev/null; then
+  echo "Homebrew already installed."
+else
+  echo "Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+eval "$(/usr/local/bin/brew shellenv 2>/dev/null || /opt/homebrew/bin/brew shellenv)"
+
+# Install apps
+brew install --cask iterm2 google-chrome obsidian visual-studio-code imazing nordvpn
+brew install node nvm
+
+# Dotfile symlinks
+# -f option will overwrite the destination file if it exists!
+ln -sf "$PWD/zshrc" "$HOME/.zshrc"
+ln -sf "$PWD/gitconfig" "$HOME/.gitconfig"
+
+echo "Install complete!"
